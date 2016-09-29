4b674eb965b14ebb1a52ab9635ac146f7092efba
Sahil Dhanju <sahildhanju1@gmail.com>
2016-09-29 01:20:40 -0500

    trying to push to heroku

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var EventEmitter = require("events");
+var eventEmitter = new EventEmitter.EventEmitter();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", function(req, res) {
+    console.log(req.body);
+    res.json({foo: "bar"});
+    // eventEmitter.emit("data");
+});
+
+app.listen(app.get("port"), function() {
+    console.log("Listening on port", app.get("port"));
+});
+
+// var net = require('net');
+
+// var server = net.createServer(function(socket) {
+//     socket.write('Echo server\r\n');
+//     socket.pipe(socket);
+//     eventEmitter.addListener("data", function() {
+//         console.log("emitted!");
+//         socket.write('Echo server\r\n');
+//         socket.pipe(socket);
+//     });
+//     socket.on("close", function() {
+//         eventEmitter.removeAllListeners("data");
+//     });
+// });
+
+// server.listen(5000, '127.0.0.1');
